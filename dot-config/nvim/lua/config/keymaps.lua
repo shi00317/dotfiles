bba5ad3704cb5e8486c1b2664cfb1897e23eb44a
Haoyi Shi <shi00317@umn.edu>
2025-12-17 13:45:39 -0600

    remove unnecessary keybind

--- dot-config/nvim/lua/config/keymaps.lua
+++ dot-config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
-local map = vim.keymap.set
-map("n", "SS", "<cmd>w<CR>", { desc = "Save Buffer", silent = true })
-map("n", "QQ", "<cmd>q<CR>", { desc = "Quit Buffer", silent = true })
---
